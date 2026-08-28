67dea51929a0b72e75ad138ca45af4bb56df2c5d
Joydeep-Sadhukhan <joysadhukhan13@gmail.com>
2026-08-28 22:58:14 +0530

    fronted url added to cors

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(cors(
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
+    "http://64.20.56.247:5174",
     // add production url
   ],
   credentials: true,
